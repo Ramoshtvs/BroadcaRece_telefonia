75a4f21668c329b36f22cc06a09cc5b7983a81da
Ramoshtvs <44620536+Ramoshtvs@users.noreply.github.com>
2021-02-24 19:46:53 -0600

    creacion del proyecto
    creacion del proyecto

---
+++ MyBroadcaReceiver_telefonia/app/src/main/java/com/example/mybroadcareceiver_telefonia/BroadcastReceiver_llamada.java
@@ -0,0 +1,14 @@
+package com.example.mybroadcareceiver_telefonia;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BroadcastReceiver_llamada extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
